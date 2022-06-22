a7bd0f2297ef8f480eb75a25ba6ca31c8f5a7575
SunDrop1 <boxbox.albert53@gmail.com>
2022-06-22 16:39:58 +0100

    First Commit

---
+++ ExamBackend/exambackend_2/examportal/src/main/java/com/exam/examportal/ExamportalApplication.java
@@ -0,0 +1,36 @@
+package com.exam.examportal;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.exam.examportal.helper.UserFoundException;
+import com.exam.examportal.model.Role;
+import com.exam.examportal.model.User;
+import com.exam.examportal.model.UserRole;
+import com.exam.examportal.service.UserService;
+
+@SpringBootApplication
+@CrossOrigin(origins = "*")
+public class ExamportalApplication implements CommandLineRunner{
+	
+	@Autowired
+	UserService userService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ExamportalApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+
+		
+	}
+
+}
